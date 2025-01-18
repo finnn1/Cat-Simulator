3f0dfd70c32405f22beb9687572640a604445869
LCU97 <saparationlee@gmail.com>
2025-01-19 01:38:42 +0900

    01-19
    아이템 매니저를 추가

---
+++ Source/CatSimul/Public/LCU/Inventory/Item/ItemManager.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemManager.generated.h"
+
+class AUItem;
+
+USTRUCT(BlueprintType)
+struct FItemRespawnData
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FVector Location; // 아이템 위치
+
+	UPROPERTY()
+	TSubclassOf<AUItem> ItemClass; // 아이템 클래스
+
+	UPROPERTY()
+	float RespawnTime; // 리스폰 타이머
+};
+
+UCLASS()
+class CATSIMUL_API AItemManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AItemManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	
+	// 아이템 줍기 함수
+	void CheckRespawnItem(AUItem* Item);
+
+private:
+	// 리스폰 데이터를 저장할 맵
+	TMap<FName, FItemRespawnData> RespawnData;
+
+	// 리스폰 처리 함수
+	void RespawnItem(FName ItemID);
+};
